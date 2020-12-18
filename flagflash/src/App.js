0181d4476c82533e3f092786b2f719a3b4d83019
Naveen-HN <hnnaveen93@gmail.com>
2020-12-18 17:30:41 -0500

    added header

--- flagflash/src/App.js
+++ flagflash/src/App.js
@@ -1,7 +1,15 @@
-import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <link
+        rel="stylesheet"
+        href="https://bootswatch.com/4/cerulean/bootstrap.min.css"
+      />
+      <Header />
+    </div>
+  );
 }
 
 export default App;
