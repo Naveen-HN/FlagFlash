3ff5318a4af7c37d47ac856f8a8060645028b451
Naveen-HN <hnnaveen93@gmail.com>
2020-12-19 01:29:13 -0500

    Added flags and components

--- flagflash/src/components/Flags.js
+++ flagflash/src/components/Flags.js
@@ -40,22 +40,116 @@ class Flags extends Component {
           name: "Algeria",
         },
       },
+      {
+        emoji: "🇦🇸",
+        emojiUnicode: "U+1F1E6 U+1F1F8",
+        svgFile: "https://restcountries.eu/data/asm.svg",
+        country: {
+          name: "American Samoa",
+          capital: "Pago Pago",
+        },
+      },
+      {
+        emoji: "🇦🇩",
+        emojiUnicode: "U+1F1E6 U+1F1E9",
+        svgFile: "https://restcountries.eu/data/and.svg",
+        country: {
+          name: "Andorra",
+          capital: "Andorra la Vella",
+        },
+      },
+      {
+        emoji: "🇦🇴",
+        emojiUnicode: "U+1F1E6 U+1F1F4",
+        svgFile: "https://restcountries.eu/data/ago.svg",
+        country: {
+          name: "Angola",
+          capital: "Luanda",
+        },
+      },
+      {
+        emoji: "🇦🇮",
+        emojiUnicode: "U+1F1E6 U+1F1EE",
+        svgFile: "https://restcountries.eu/data/aia.svg",
+        country: {
+          name: "Anguilla",
+          capital: "The Valley",
+        },
+      },
+      {
+        emoji: "🇦🇶",
+        emojiUnicode: "U+1F1E6 U+1F1F6",
+        svgFile: "https://restcountries.eu/data/ata.svg",
+        country: {
+          name: "Antarctica",
+          capital: "",
+        },
+      },
+      {
+        emoji: "🇦🇬",
+        emojiUnicode: "U+1F1E6 U+1F1EC",
+        svgFile: "https://restcountries.eu/data/atg.svg",
+        country: {
+          name: "Antigua and Barbuda",
+          capital: "Saint John's",
+        },
+      },
+      {
+        emoji: "🇦🇷",
+        emojiUnicode: "U+1F1E6 U+1F1F7",
+        svgFile: "https://restcountries.eu/data/arg.svg",
+        country: {
+          name: "Argentina",
+          capital: "Buenos Aires",
+        },
+      },
+      {
+        emoji: "🇦🇲",
+        emojiUnicode: "U+1F1E6 U+1F1F2",
+        svgFile: "https://restcountries.eu/data/arm.svg",
+        country: {
+          name: "Armenia",
+          capital: "Yerevan",
+        },
+      },
     ],
   };
+
   render() {
-    return this.state.Flag.map((flag) => (
-      <Card
-        image={flag.svgFile}
-        name={flag.country.name}
-        capital={flag.country.capital}
-      />
-    ));
-    //   <Card
-    //     image={this.state.Flag[0].svgFile}
-    //     name={this.state.Flag[0].country.name}
-    //     capital={this.state.Flag[0].country.capital}
-    //   />
+    return (
+      <div
+        style={{
+          margin: "0px 10px 0px",
+          display: "flex",
+          justifyContent: "space-between",
+          flexWrap: "wrap",
+          alignContent: "flex-start",
+        }}
+      >
+        {randomArrayShuffle(this.state.Flag).map((flag) => (
+          <Card
+            image={flag.svgFile}
+            name={flag.country.name}
+            capital={flag.country.capital}
+          />
+        ))}
+      </div>
+    );
   }
+}
+
+function randomArrayShuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
 }
 
 export default Flags;
--- flagflash/src/index.js
+++ flagflash/src/index.js
@@ -5,6 +5,10 @@ import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
+    <link
+      rel="stylesheet"
+      href="https://bootswatch.com/4/cerulean/bootstrap.min.css"
+    />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
