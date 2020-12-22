a77c99b793a97f1dbe712e53fa0ab478fcb3a00f
Naveen-HN <hnnaveen93@gmail.com>
2020-12-21 19:21:06 -0500

    Made Card.js as a functional component

--- flagflash/src/components/Card.js
+++ flagflash/src/components/Card.js
@@ -1,102 +1,88 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import ReactCardFlip from "react-card-flip";
 
-class Card extends Component {
-  state = {
-    isFlipped: false,
-  };
+export default function Card(props) {
+  const [isFlipped, setIsFlipped] = useState(false);
 
-  handleClick = (e) => {
-    e.preventDefault();
-    this.setState((prevState) => ({ isFlipped: !prevState.isFlipped }));
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.isFlipped) {
-      this.flipTimeout = setTimeout(() => {
-        this.setState(() => ({ isFlipped: false }));
+  useEffect(() => {
+    let flipTimeout;
+    if (isFlipped) {
+      flipTimeout = setTimeout(() => {
+        setIsFlipped(false);
       }, 5000);
     }
-  }
-
-  componentWillUnmount() {
-    clearTimeout(this.flipTimeout);
-  }
-
-  render() {
-    return (
-      <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical">
-        <div
-          className="card bg-secondary mb-3"
+    return () => clearTimeout(flipTimeout);
+  }, [isFlipped]);
+  return (
+    <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+      <div
+        className="card bg-secondary mb-3"
+        style={{
+          width: "20rem",
+          height: "13.6rem",
+          maxWidth: "20rem",
+          maxHeight: "13.6rem",
+          boxShadow:
+            "0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+        }}
+        onClick={() => setIsFlipped(!isFlipped)}
+      >
+        <img
+          src={props.image}
           style={{
             width: "20rem",
             height: "13.6rem",
             maxWidth: "20rem",
             maxHeight: "13.6rem",
-            boxShadow:
-              "0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0, 0, 0, 0.19)",
           }}
-          onClick={this.handleClick}
-        >
-          <img
-            src={this.props.image}
-            style={{
-              width: "20rem",
-              height: "13.6rem",
-              maxWidth: "20rem",
-              maxHeight: "13.6rem",
-            }}
-            alt={this.props.name}
-          />
-        </div>
+          alt={props.name}
+        />
+      </div>
 
-        <div
-          className="card"
-          style={{
-            width: "20rem",
-            height: "13.6rem",
-            maxWidth: "20rem",
-            maxHeight: "13.6rem",
-            boxShadow:
-              "0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0, 0, 0, 0.19)",
-          }}
-          onClick={this.handleClick}
-        >
-          <div className="card-body">
-            <h5 className="card-title" style={{ textAlign: "center" }}>
-              {this.props.name}
-            </h5>
-            <h6
-              className="card-subtitle mb-2 text-muted"
-              style={{ textAlign: "center" }}
-            >
-              Capital: {this.props.capital}
-            </h6>
-            <p className="card-text">
-              Population: {this.props.population}
-              <br />
-              Official Languages:{" "}
-              {this.props.officialLanguage.map((lang, index) => (
-                <span key={index} className="badge badge-primary">
-                  {lang.name}
-                </span>
-              ))}
-              <br />
-              Currency:
-              <span className="badge badge-pill badge-success">
-                {this.props.currency.map((sym) => sym.symbol)}
+      <div
+        className="card"
+        style={{
+          width: "20rem",
+          height: "13.6rem",
+          maxWidth: "20rem",
+          maxHeight: "13.6rem",
+          boxShadow:
+            "0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+        }}
+        onClick={() => setIsFlipped(!isFlipped)}
+      >
+        <div className="card-body">
+          <h5 className="card-title" style={{ textAlign: "center" }}>
+            {props.name}
+          </h5>
+          <h6
+            className="card-subtitle mb-2 text-muted"
+            style={{ textAlign: "center" }}
+          >
+            Capital: {props.capital}
+          </h6>
+          <p className="card-text">
+            Population: {props.population}
+            <br />
+            Official Languages:{" "}
+            {props.officialLanguage.map((lang, index) => (
+              <span key={index} className="badge badge-primary">
+                {lang.name}
               </span>
-              {this.props.currency.map((curr, index) => (
-                <span key={index} className="badge badge-pill badge-info">
-                  {curr.name}
-                </span>
-              ))}
-            </p>
-          </div>
+            ))}
+            <br />
+            Currency:
+            <span className="badge badge-pill badge-success">
+              {props.currency.map((sym) => sym.symbol)}
+            </span>
+            {props.currency.map((curr, index) => (
+              <span key={index} className="badge badge-pill badge-info">
+                {curr.name}
+              </span>
+            ))}
+          </p>
         </div>
-      </ReactCardFlip>
-    );
-  }
+      </div>
+    </ReactCardFlip>
+  );
 }
-
-export default Card;
